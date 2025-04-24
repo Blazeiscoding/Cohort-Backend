0a55bcec7ace8e6e1da941d40a8180817814c75c
Nikhil Rathore <nikhilrathore52297@gmail.com>
2025-04-24 10:18:58 +0530

    Refactor app.js to include user routes and correct import syntax

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 
 //router imports
-import healthCheckRouter from "./routes/healthcheck.routes.js"
-
-app.use("/api/v1/healthcheck", healthCheckRouter)
-
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import router from "./routes/auth.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/users", router);
 export default app;
